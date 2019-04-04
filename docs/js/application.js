25fcccf4476348d4892c5a06ac34e927ef76745f
Aleksandr Ignatev <vigntom@gmail.com>
2019-04-04 11:43:58 +0300

    Update docs

--- docs/js/application.js
+++ docs/js/application.js
@@ -1,16 +1,18 @@
-"use strict";
-
 /* globals Modernizr */
-(function app() {
-  var $root = document.querySelector('html');
-  $root.classList.remove('no-js');
-  Modernizr.on('webp', function (result) {
-    if (result) {
-      $root.classList.add('webp');
-    }
-
-    if (result.lossless) {
-      $root.classList.add('webp-lossless');
-    }
-  });
-})();
+// (function app () {
+//   const $root = document.querySelector('html')
+//   $root.classList.remove('no-js')
+//   Modernizr.on('webp', result => {
+//     if (result) {
+//       $root.classList.add('webp')
+//     } else {
+//       $root.classList.add('no-webp')
+//     }
+//     if (result.lossless) {
+//       $root.classList.add('webp-lossless')
+//     } else {
+//       $root.classList.add('no-webp-lossless')
+//     }
+//   })
+// }())
+"use strict";
